9681c348d6693d9f7dc1cb5ca829fc671fcf8705
asifanwar007 <asifanwar0000@outlook.com>
2024-05-12 11:46:58 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+python3 -m venv env
+pip install -r requirements.txt
+python3 main.py
+
+
+# github
+
+.gitignore
+commit the
---
+++ main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("main.html", {"request": request})
+
+@app.post("/check-palindrome/", response_class=HTMLResponse)
+async def check_palindrome(request: Request):
+    form_data = await request.form()
+    word = form_data["word"]
+    is_palindrome = word == word[::-1]
+    return templates.TemplateResponse("result.html", {"request": request, "word": word, "is_palindrome": is_palindrome})
+
+if __name__ == "__main__":
+    import uvicorn
+    try:
+       uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    except Exception as e:
+        print(e)
